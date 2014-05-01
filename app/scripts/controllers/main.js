1db97357da7d7b9dca828185957ea3f9ae5d6a78
Fabricio Quagliariello <matias.quaglia@gmail.com>
2014-05-01 10:10:37 -0300

    #8 made the qualities selector work

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,7 @@
 
 angular.module('trianguloApp')
   .controller('MainCtrl', function ($scope) {
+
     $scope.tones = [
       'C',
       'C#',
@@ -27,10 +28,24 @@ angular.module('trianguloApp')
       'Minor'
     ];
 
-
+    $scope.triads = {
+      'Major': [
+         ['T', 'M3', 'P5'],
+         ['M-3', 'T', 'm3'],
+         ['P-5', 'm-3', 'T']
+      ],
+      'Minor': [
+         ['T', 'm3', 'P5'],
+         ['m-3', 'T', 'M3'],
+         ['P-5', 'M-3', 'T']
+      ]
+    };
 
     $scope.selectedQuality = $scope.qualities[0];
     $scope.selectedTone = $scope.tones[0];
 
+    $scope.$watch('selectedQuality', function() {
+      $scope.$broadcast('qualityChanged');
+    });
 
   });
