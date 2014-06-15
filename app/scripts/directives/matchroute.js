5c89836ac4aafefead574261394666d365a6c337
Fabricio Quagliariello <matias.quaglia@gmail.com>
2014-06-15 19:45:02 -0300

    fixed #18 added basic preloader

--- app/scripts/directives/matchroute.js
+++ app/scripts/directives/matchroute.js
@@ -1,13 +1,15 @@
 'use strict';
 
 angular.module('trianguloApp')
-  .directive('matchRoute', function ($location) {
+  .directive('matchRoute', function ($route) {
     return {
       restrict: 'A',
       link: function postLink(scope, element, attrs) {
         var matchClass = attrs.matchRouteClass || 'active';
         scope.$on('$routeChangeSuccess', function() {
-          var match = attrs.matchRoute === $location.path();
+          var match = angular.isObject($route.current) &&
+                      angular.isString($route.current.originalPath) &&
+                      attrs.matchRoute === $route.current.originalPath;
           element.toggleClass(matchClass, match);
         });
       }
