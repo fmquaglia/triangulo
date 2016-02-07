f7aa00de802c1ae017af302870b376341ac9159f
Fabricio Matías Quagliariello <matias.quaglia@gmail.com>
2016-02-07 16:07:42 -0300

    added midi number

--- app/scripts/directives/note.js
+++ app/scripts/directives/note.js
@@ -7,7 +7,7 @@ angular.module('trianguloApp')
         key: '=',
         transpose: '&'
       },
-      template: '<span class="note" ng-click="play()">' +
+      template: '<span class="note midi-{{midiNumber}}" ng-click="play()">' +
                   '<span class="title" ng-transclude></span>' +
                   '<span class="note_text">' +
                     '<span class="name">{{name}}</span>' +
@@ -27,24 +27,29 @@ angular.module('trianguloApp')
           scope.key = 'A';
         }
         var parseKey = function() {
-          unTransposedNote = Teoria.note.fromString(scope.key);
+          unTransposedNote  = Teoria.note.fromString(scope.key);
           if (scope.transpose() && angular.isString(scope.transpose())) {
-            transposedNote = Teoria.note.fromString(scope.key).transpose(scope.transpose());
+            transposedNote  = Teoria.note.fromString(scope.key).transpose(scope.transpose());
           } else {
-            transposedNote = unTransposedNote;
+            transposedNote  = unTransposedNote;
           }
-          scope.name = transposedNote.name().toUpperCase();
-          scope.accidental = transposedNote.accidental().toLowerCase();
+          scope.name        = transposedNote.name().toUpperCase();
+          scope.accidental  = transposedNote.accidental().toLowerCase();
+          scope.midiNumber  = getMidiNumber(transposedNote);
           scope.enharmonics = _.map(transposedNote.enharmonics(), function(enharmonic) {
             return enharmonic.name().toUpperCase() + enharmonic.accidental().toLowerCase();
           });
         };
 
+        var getMidiNumber = function (note){
+          return $filter('midiNumber')(note.key());
+        };
+
         var playMIDI = function(note) {
-          note = note || transposedNote;
-          var midiNumber = $filter('midiNumber')(note.key());
-          var delay = 0;
-          var velocity = 127;
+          note            = note || transposedNote;
+          var midiNumber  = getMidiNumber(note);
+          var delay       = 0;
+          var velocity    = 127;
           MIDI.setVolume(0, 127);
           MIDI.noteOn(0, midiNumber, velocity, delay);
           MIDI.noteOff(0, midiNumber, delay + 0.75);
