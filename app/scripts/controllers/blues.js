64175671bd620676c705c75431e46ba62a7f6298
Fabricio Matías Quagliariello <matias.quaglia@gmail.com>
2016-02-07 19:26:54 -0300

    #46 add elements to view

--- app/scripts/controllers/blues.js
+++ app/scripts/controllers/blues.js
@@ -1,8 +1,31 @@
 'use strict';
 
 angular.module('trianguloApp')
-  .controller('BluesCtrl', function ($scope, Tones) {
+  .controller('BluesCtrl', function ($scope, $route, Tones) {
+    var quality = $route.current.quality;
+    var majorBluesTranspositions = [
+      'P1',
+      'M2',
+      'm3', // blue note
+      'M3',
+      'P5',
+      'M6'
+    ];
+    var minorBluesTranspositions = [
+      'P1',
+      'm3',
+      'P4',
+      'd5', // blue note
+      'P5',
+      'm7'
+    ];
+    var transpositions = {
+      'major': majorBluesTranspositions,
+      'minor': minorBluesTranspositions
+    };
+
     $scope.tones = Tones;
     $scope.selectedTone = $scope.tones[0];
+    $scope.transpositions = transpositions[quality] || transpositions['major'];
 
   });
